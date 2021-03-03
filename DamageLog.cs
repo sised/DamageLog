9ac008bcd899a8873c141c8e9f645487da25da1c
sised <63566157+sised@users.noreply.github.com>
2021-03-03 18:43:13 +0200

    added the entire observable universe

---
+++ DamageLog.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace DamageLog
+{
+	public class DamageLog : Mod
+	{
+	}
+}
---
+++ Properties/Mplayer.cs
@@ -0,0 +1,36 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria.DataStructures;
+
+namespace DamageLog.Properties
+{
+    public class Mplayer : ModPlayer
+    {
+        public override void OnHitByNPC(NPC npc, int damage, bool crit)
+        {
+            Main.NewText(player.name + " got hit by " + npc.FullName + ", " + damage.ToString() + " Damage!", Color.IndianRed);
+        }
+        public override void OnHitByProjectile(Projectile proj, int damage, bool crit)
+        {
+            Main.NewText(player.name + " got hit by " + proj.Name + ", " + damage.ToString() + " Damage!", Color.IndianRed);
+        }
+        public override bool PreHurt(bool pvp, bool quiet, ref int damage, ref int hitDirection, ref bool crit, ref bool customDamage, ref bool playSound, ref bool genGore, ref PlayerDeathReason damageSource)
+        {
+            if(damageSource.SourceOtherIndex == 0)
+            {
+                Main.NewText(player.name + " fell, " + damage.ToString() + " Damage!", Color.IndianRed);
+            }
+            if (damageSource.SourceOtherIndex == 2)
+            {
+                Main.NewText(player.name + " got burned in lava, " + damage.ToString() + " Damage!", Color.IndianRed);
+            }
+            return true;
+        }
+        public override void OnHitPvp(Item item, Player target, int damage, bool crit)
+        {
+            Main.NewText(target.name + " got hit by " + player.name + ", " + damage.ToString() + " Damage!", Color.IndianRed);
+        }
+    }
+}
